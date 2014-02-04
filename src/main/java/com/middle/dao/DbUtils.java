4b032327cdeffe4d15135ff997f84f8ee4be9d1f
asettouf <adescarte@etu.emse.fr>
2014-02-04 22:49:16 +0100

    Adding a test of persistence in main

--- src/main/java/com/middle/dao/DbUtils.java
+++ src/main/java/com/middle/dao/DbUtils.java
@@ -1,13 +1,21 @@
 package com.middle.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+
 public class DbUtils {
 	
-	private final String uri="Something";
+	@PersistenceContext
+	private static EntityManager em;
 	//private final String driver="other thing";
+	
 
 	public DbUtils() {
 		// TODO Auto-generated constructor stub
 	}
+	
+
 	
 	
 
--- src/main/java/com/middle/main/Main.java
+++ src/main/java/com/middle/main/Main.java
@@ -1,6 +1,17 @@
 package com.middle.main;
 
+import javax.ejb.embeddable.EJBContainer;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+
+import com.middle.domain.Release;
+
 public class Main {
+	
+	@PersistenceContext
+	private static EntityManager em;
 
 	public Main() {
 		// TODO Auto-generated constructor stub
@@ -8,6 +19,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("toto",System.getProperties());
+		em=emf.createEntityManager();
+		em.getTransaction().begin();
+		Release r=new Release();
+		r.setReleaseNumber(1);
+		
+		em.persist(r);
+		em.getTransaction().commit();
+		//System.out.println("yeah");
 
 	}
 
